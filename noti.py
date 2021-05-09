c0b3953c49fd8a1438030502cfed96cebed474ca
Natchapol Srimuang <natchapol.sr@outlook.com>
2021-05-09 17:49:54 +0700

    add line token

--- noti.py
+++ noti.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def line_send(message):
@@ -10,7 +11,11 @@ def line_send(message):
 
 def get_message(payload):
     url = 'https://notify-api.line.me/api/notify'
-    token = 'LmZF8P5RxyEASoVL1Hinna1HUc04E7OBtC2nlYVl3Ub'
+    
+    with open('_keys/line_token.json') as token_file:
+        token_dict = json.load(token_file)
+    token = token_dict['tradebot']
+    
     headers = {'Authorization':'Bearer ' + token}
     
     return requests.post(url, headers = headers , data = payload)
