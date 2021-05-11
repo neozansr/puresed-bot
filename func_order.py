1baaa54d5f721285e8250b7e3cbfecfec370a866
Natchapol Srimuang <natchapol.sr@outlook.com>
2021-05-11 23:41:18 +0700

    use n_sell_order for latest and latest - grid

--- func_order.py
+++ func_order.py
@@ -91,12 +91,15 @@ def open_buy_orders(exchange, n_order, n_sell_order, n_open_order, symbol, grid,
     open_buy_orders_df = open_orders_df[open_orders_df['side'] == 'buy']
     open_sell_orders_df = open_orders_df[open_orders_df['side'] == 'sell']
     
-    min_open_sell_price = min(open_sell_orders_df['price'], default = 0)
-    gap_sell = latest_price - min_open_sell_price
+    max_open_buy_price = max(open_buy_orders_df['price'], default = 0)
 
-    if  gap_sell > grid:
+    if latest_price - max_open_buy_price > grid:
         open_orders_df, transactions_df = cancel_open_buy_orders(exchange, symbol, grid, latest_price, fee_percent, open_orders_df, transactions_df)
-        buy_price_list = cal_new_orders(n_order, n_sell_order, grid, latest_price, start_market)
+        
+        if len(open_sell_orders_df) == 0:
+            buy_price_list = cal_new_orders(n_order, n_sell_order, grid, latest_price, start_market)
+        else:    
+            buy_price_list = cal_new_orders(n_order, n_sell_order, grid, latest_price, start_market = 0)
     else:
         buy_price_list = cal_append_orders(n_order, n_open_order, grid, open_buy_orders_df)
 
