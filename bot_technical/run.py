ef6e306308fcb3f6ee02a61df28bc3df7fa245c5
green-nk <green.nk21@gmail.com>
2022-01-21 21:39:07 +0700

    Draft close position checking process

--- bot_technical/run.py
+++ bot_technical/run.py
@@ -9,10 +9,10 @@ sys.path.append(os.path.abspath(src_path))
 
 from func_get import get_json, get_time, get_exchange, check_end_date
 from func_update import append_error_log, update_timestamp
-from  func_technical import update_end_date_technical, check_current_position, open_position
+from  func_technical import update_end_date_technical, check_close_position, check_open_position
 
 
-def run_bot():
+def run_bot(config_system, config_params_path, last_loop_path, cash_flow_df_path, transactions_df_path, profit_df_path):
     bot_name = os.path.basename(os.getcwd())
     exchange = get_exchange(config_system, future=True)
 
@@ -21,8 +21,8 @@ def run_bot():
     if end_date_flag == 1:
         update_end_date_technical()
 
-    check_current_position()
-    open_position()
+    check_close_position(exchange, config_params_path, last_loop_path, transactions_df_path, profit_df_path)
+    check_open_position()
         
     timestamp = get_time()
     print(f"Time: {timestamp}")
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         if config_system['run_flag'] == 1:
             print("Start loop")
             try:
-                run_bot()
+                run_bot(config_system, config_params_path, last_loop_path, cash_flow_df_path, transactions_df_path)
             except (ccxt.RequestTimeout, ccxt.NetworkError, ccxt.ExchangeError):
                 append_error_log('ConnectionError', error_log_df_path)
                 print("No connection: Skip the loop")
--- src/func_technical.py
+++ src/func_technical.py
@@ -1,3 +1,10 @@
+import pandas as pd
+
+from func_get import get_json, get_time, get_order_fee
+from func_cal import cal_adjusted_price
+from func_update import append_order
+
+
 def get_ohlcv():
     '''
     Fetch candle stick from exchange at the longest windows signal
@@ -33,12 +40,26 @@ def get_signal():
     add_wt_cross()
 
 
-def append_profit_technical():
+def append_profit_technical(exchange, order, last_loop_dict, profit_df_path):
     '''
     Cal profit from close order.
     Record profit on profit file.
     '''
-    pass
+    profit_df = pd.read_csv(profit_df_path)
+
+    timestamp = get_time()
+    order_id = order['id']    
+    symbol = order['symbol']
+    side = order['side']
+    amount = order['amount']
+    
+    fee = get_order_fee(order, exchange, symbol)
+    open_price = last_loop_dict[symbol]['open_price']
+    close_price = cal_adjusted_price(order, fee, side)
+    profit = (close_price - open_price) * amount       
+
+    profit_df.loc[len(profit_df)] = [timestamp, order_id, symbol, side, amount, open_price, close_price, fee, profit]
+    profit_df.to_csv(profit_df_path, index=False)
 
 
 def cal_budget_technical():
@@ -69,13 +90,18 @@ def check_lead_symbol_signal(timeframe):
     pass
 
 
-def check_close_position_condition():
+def check_close_signals(exchange, config_params_path, position):
     '''
     Close position if one of criteria occur.
     Return clost_position flag.
     '''
-    get_signal()
-    pass
+    is_close_position = False
+    price_signal, supertrend_signal, wt_cross_signal = get_signal(exchange, config_params_path, position)
+
+    if price_signal or supertrend_signal or wt_cross_signal:
+        is_close_position = True        
+
+    return is_close_position
 
 
 def check_long_entry_position_condition():
@@ -96,7 +122,37 @@ def check_short_entry_position_condition():
     pass
 
 
-def check_current_position():
+def close_position(exchange, symbol, action, last_loop_dict):
+    '''
+    '''
+    amount = last_loop_dict['symbols'][symbol]
+    order = exchange.create_order(symbol, 'market', action, amount)
+
+    return order
+
+
+def create_action(position, to_close=True):
+    '''
+    '''
+    if (to_close and position == 'long') or (not to_close and position == 'short'):
+        action = 'sell'
+    elif (to_close and position == 'short') or (not to_close and position == 'long'):
+        action = 'buy'
+    else:
+        raise ValueError('position should be long or short only!!!')
+
+    return action
+
+
+def clear_position_symbol(symbol, last_loop_dict):
+    '''
+    Remove position of symbol before entering
+    a new position.
+    '''
+    last_loop_dict.pop(symbol)
+
+
+def check_close_position(exchange, config_params_path, last_loop_path, transactions_df_path, profit_df_path):
     '''
     Check all coins in last_loop
     if exist:
@@ -112,10 +168,24 @@ def check_current_position():
 
     Remove symbol from last_loop
     '''
-    pass
+    last_loop = get_json(last_loop_path)
+    symbols = list(last_loop['symbols'].keys())
+
+    for symbol in symbols:
+        position = symbol['position']
+        is_close_position = check_close_signals(exchange, config_params_path, position)
+            
+        if is_close_position:
+            action = create_action(position, to_close=True)
+            order = close_position(exchange, symbol, action, last_loop)
+
+            append_order(order, 'filled', transactions_df_path)    
+            append_profit_technical(exchange, order, last_loop, profit_df_path)
+
+            clear_position_symbol(symbol, last_loop) 
 
 
-def open_position():
+def check_open_position():
     n_order = cal_budget_technical()
     coin_list = get_no_posion_symbol()
 
