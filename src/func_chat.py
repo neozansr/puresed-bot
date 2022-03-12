b1160b2152f44e18cc8bc1f94c63b2e2926d2dcb
neozan <natchapol.sr@outlook.com>
2022-03-13 01:11:39 +0700

    fix grid chatbot transactions_df_path

--- src/func_chat.py
+++ src/func_chat.py
@@ -135,7 +135,7 @@ def get_grid_text(home_path, bot_name, bot_type, config_system_path, config_para
     min_buy_price, max_buy_price, min_sell_price, max_sell_price = get_pending_order(bot_path + open_orders_df_path)
 
     cur_date = get_date()
-    today_cash_flow = get_cash_flow_grid(cur_date, config_params, transactions_df_path)
+    today_cash_flow = get_cash_flow_grid(cur_date, config_params, bot_path + transactions_df_path)
     funding_payment, _ = get_funding_payment(exchange, range='today')
 
     last_loop = get_json(bot_path + last_loop_path)
