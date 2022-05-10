b2d521b270c14b85a118738fb1987c1d55b116db
neozan <natchapol.sr@outlook.com>
2022-05-11 00:35:05 +0700

    add cal_end_balance description

--- src/func_cal.py
+++ src/func_cal.py
@@ -32,9 +32,11 @@ def cal_adjusted_price(order, fee, side):
 
 
 def cal_end_balance(base_currency_value, quote_currency_value, transfer):
-    # This function is called before update, today withdraw is still in withdraw.
-    # After update, withdraw is moved to pending_withdraw and will be edited manually after tranfer fund.
-    # Deposit has already been included in balance as it is added before end of the day.
+    '''
+    This function is called before update, today withdraw is still in withdraw.
+    After update, withdraw is moved to pending_withdraw and will be edited manually after tranfer fund.
+    Deposit has already been included in balance as it is added before end of the day.
+    '''
     end_balance = base_currency_value + quote_currency_value - transfer['withdraw'] - transfer['pending_withdraw']
 
     return end_balance
